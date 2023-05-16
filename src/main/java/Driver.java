27493f1ea5ed48622a1fe9c6097617a836ff2199
bayuevri <100820224+bayuevri@users.noreply.github.com>
2023-05-16 18:28:07 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,21 +1,34 @@
-import src.main.java.Counter;
+class Counter {
+    private int count;
 
-public class Driver {
+    public Counter() {
+        count = 0;
+    }
 
-	public static void main(String[] args) {
-		
-		Counter counter = new Counter();
-		
-		System.out.println("Count:" + counter.getCount());
-		
-		counter.increment();
-		
-		System.out.println("Count:" + counter.getCount());
-		
-		counter.decrement();
-		
-		System.out.println("Count:" + counter.getCount());
-		
-	}
+    public int getCount() {
+        return count;
+    }
 
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+
+        System.out.println("Count: " + counter.getCount());
+
+        counter.increment();
+
+        System.out.println("Count: " + counter.getCount());
+
+        counter.decrement();
+
+        System.out.println("Count: " + counter.getCount());
+    }
