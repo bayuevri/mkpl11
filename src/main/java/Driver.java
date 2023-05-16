ad75e662e0ca15a317af340e23f25890135c038c
bayuevri <100820224+bayuevri@users.noreply.github.com>
2023-05-16 18:33:40 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,23 +1,4 @@
 class Counter {
-    private int count;
-
-    public Counter() {
-        count = 0;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void increment() {
-        count++;
-    }
-
-    public void decrement() {
-        count--;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         Counter counter = new Counter();
